a8074cec04c8ac8882046f7a81491decbe0117d8
sangya2001 <sangyasherpa2058@gmail.com>
2020-11-07 18:53:46 +0545

    Mixed minor bugs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import * as Util from './utils'
+
+// peer dependencies
 import { createAlchemyWeb3 } from "@alch/alchemy-web3";
 import Web3 from "web3";
 
+// exporting package
 export const CleanContract = (apiKey, contractJson, contractAddress) => {
+  // empty parameter handling
   if (!apiKey || !contractJson || !contractAddress) {
     Util.setErrorMessage("One of the parameters for OpenContract is missing!");
     return { errorMessage: Util.getErrorMessage() };
   }
 
+  // connect wallet
   async function loadWeb3() {
     var { ethereum, web3 } = window
 
@@ -25,6 +30,7 @@ export const CleanContract = (apiKey, contractJson, contractAddress) => {
     Util.setAccount(accounts[0])
   }
 
+  // load contract
   async function loadContract(apiKey, contractJson, contractAddress) {
     // setup contract
     const alchWeb3 = createAlchemyWeb3(apiKey)
@@ -38,10 +44,12 @@ export const CleanContract = (apiKey, contractJson, contractAddress) => {
     Util.setContractState(false)
   }
 
+  // calling setup
   (async function fetchData() {
     await loadWeb3()
     await loadContract(apiKey, contractJson, contractAddress)
   })()
 
+  // returning value
   return { account: Util.getAccount(), contract: Util.getContract(), contractState: Util.getContractState };
 }
